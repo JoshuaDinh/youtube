2bc75011345d4ddb6cd80384c2d9a50eb9880496
Joshua Dinh <joshuahungdinh@gmail.com>
2021-05-21 14:56:56 -0700

    updated overlay styling and title background

--- src/Components/VideoCard/VideoCard.js
+++ src/Components/VideoCard/VideoCard.js
@@ -5,7 +5,6 @@ import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 const VideoCard = ({ title, thumbnail, setVideoId, videoId, channel }) => {
   return (
     <div className="video-card">
-      {/* <PlayCircleOutlineIcon className="play-icon" /> */}
       <p className="video-card-title">{title}</p>
       <img src={thumbnail} alt="#" className="video-thumbnail" />
     </div>
