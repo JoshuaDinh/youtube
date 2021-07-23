1ad42a5855886cd71a49c36bcb22f4e9e37c1dea
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-23 11:30:24 -0700

    changed icons and routing, setup for catagories to be added

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -12,37 +12,36 @@ import SettingsIcon from "@material-ui/icons/Settings";
 import { Link } from "react-router-dom";
 import Welcome from "../Welcome/Welcome";
 
+import StorageIcon from "@material-ui/icons/Storage";
+import HttpIcon from "@material-ui/icons/Http";
+import SecurityIcon from "@material-ui/icons/Security";
+import VerticalSplitIcon from "@material-ui/icons/VerticalSplit";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      {/* <img src={youtubeIcon} className="youtube-icon" /> */}
       <Welcome />
       <div className="sidebar-section-container">
+        {" "}
         <Link to="/" className="link">
           <HomeIcon className="sidebar-icon" /> <span>Home</span>
         </Link>
+        <p>Catagories</p>
         <Link to="/" className="link">
-          <WhatshotIcon className="sidebar-icon" /> <span>Whats Hot</span>
+          <HttpIcon className="sidebar-icon" /> <span>Front-End</span>
         </Link>
         <Link to="/" className="link">
-          <SubscriptionsIcon className="sidebar-icon" />{" "}
-          <span>Subscriptions</span>
+          <HttpIcon className="sidebar-icon" /> <span>Back-End</span>
         </Link>
         <Link className="link">
-          <HistoryIcon className="sidebar-icon" /> <span>History</span>
+          <StorageIcon className="sidebar-icon" /> <span>Databases</span>
         </Link>
         <Link className="link">
-          <ThumbUpAltIcon className="sidebar-icon" /> <span>Likes</span>
+          <VerticalSplitIcon className="sidebar-icon" /> <span>Full-Stack</span>
         </Link>
         <Link className="link">
-          <PlaylistPlayIcon className="sidebar-icon" />
-          <span>Playlists</span>
-        </Link>
-        <Link className="link">
-          <AccountCircleIcon className="sidebar-icon" /> <span>Account</span>
-        </Link>
-        <Link className="link">
-          <SettingsIcon className="sidebar-icon" /> <span>Settings</span>
+          <SecurityIcon className="sidebar-icon" />
+          <span>Web Security</span>
         </Link>
       </div>
     </div>
@@ -50,3 +49,40 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+{
+  /* <Link className="link">
+          <PlaylistPlayIcon className="sidebar-icon" />
+          <span>Playlists</span>
+        </Link> */
+}
+{
+  /* <Link className="link">
+          <ThumbUpAltIcon className="sidebar-icon" /> <span>Likes</span>
+        </Link> */
+}
+{
+  /* <Link className="link">
+          <HistoryIcon className="sidebar-icon" /> <span>History</span>
+        </Link> */
+}
+{
+  /* <Link to="/" className="link">
+          <WhatshotIcon className="sidebar-icon" /> <span>Whats Hot</span>
+        </Link> */
+}
+{
+  /* <Link to="/" className="link">
+          <SubscriptionsIcon className="sidebar-icon" />{" "}
+          <span>Subscriptions</span>
+        </Link> */
+}
+
+{
+  /* <Link className="link">
+<AccountCircleIcon className="sidebar-icon" /> <span>Account</span>
+</Link>
+<Link className="link">
+<SettingsIcon className="sidebar-icon" /> <span>Settings</span>
+</Link> */
+}
