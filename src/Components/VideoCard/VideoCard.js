c45a4d75e4e59e39fde3c447e8e1ae24d95bb182
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-23 08:39:19 -0700

    updated styling

--- src/Components/VideoCard/VideoCard.js
+++ src/Components/VideoCard/VideoCard.js
@@ -1,27 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import "./videoCard.css";
 import { Link } from "react-router-dom";
 const VideoCard = ({ title, thumbnail, setVideoId, videoId }) => {
   return (
-    <div className="video-card">
-      <img src={thumbnail} alt="#" className="video-thumbnail" />{" "}
+    <Link
+      className="video-card"
+      to="/watch"
+      className="video-card"
+      onClick={() => setVideoId(videoId)}
+    >
+      {/* <div className="play-container">
+        <PlayArrowIcon className="play-icon" />
+      </div> */}
+      <img src={thumbnail} alt="#" className="video-thumbnail" />
       <div className="card-info">
         <p className="video-card-title ">{title}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 export default VideoCard;
-// const VideoCard = ({ title, thumbnail, setVideoId, videoId }) => {
-//   return (
-//     <Link
-//       to="/watch"
-//       className="video-card"
-//       onClick={() => setVideoId(videoId)}
-//     >
-//       <img src={thumbnail} alt="#" className="video-thumbnail" />
-//       <p className="video-card-title ">{title}</p>
-//     </Link>
-//   );
-// };
-// export default VideoCard;
