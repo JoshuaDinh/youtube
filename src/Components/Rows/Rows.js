9bd036d8702cd04e1f4e12d303f3c8ac567a06db
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-31 09:41:58 -0700

    added ternary to determine how many rows of videos to display based on page

--- src/Components/Rows/Rows.js
+++ src/Components/Rows/Rows.js
@@ -1,25 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./rows.css";
 import VideoCard from "../VideoCard/VideoCard";
+import { Link } from "react-router-dom";
 
-const Rows = ({ videos, setVideoId, title }) => {
+const Rows = ({ videos, setVideoId, title, link, homepage }) => {
+  // Displays Link if user is on the homepage
+  const viewAllBtn = () => {
+    if (window.location.pathname === "/") {
+      return (
+        <Link to={link}>
+          <div className="view-link">View All</div>
+        </Link>
+      );
+    }
+  };
+
   return (
     <div className="rows">
       <h5 className="rows-title">{title}</h5>
+      {viewAllBtn()}
       <div className="rows-card-container">
-        {videos.slice(1).map((data) => {
-          return (
-            data.snippet?.thumbnails && (
-              <VideoCard
-                setVideoId={setVideoId}
-                videoId={data.videoId.videoId}
-                title={data.snippet?.title}
-                thumbnail={data.snippet?.thumbnails?.medium.url}
-                channel={data.snippet?.channelTitle}
-              />
-            )
-          );
-        })}{" "}
+        {/* Determine how many videos to display */}
+        {homepage
+          ? videos?.slice(1, 5).map((data) => {
+              return (
+                data.snippet?.thumbnails && (
+                  <VideoCard
+                    setVideoId={setVideoId}
+                    videoId={data.videoId.videoId}
+                    title={data.snippet?.title}
+                    thumbnail={data.snippet?.thumbnails?.medium.url}
+                    channel={data.snippet?.channelTitle}
+                  />
+                )
+              );
+            })
+          : videos?.slice(1).map((data) => {
+              return (
+                data.snippet?.thumbnails && (
+                  <VideoCard
+                    setVideoId={setVideoId}
+                    videoId={data.videoId.videoId}
+                    title={data.snippet?.title}
+                    thumbnail={data.snippet?.thumbnails?.medium.url}
+                    channel={data.snippet?.channelTitle}
+                  />
+                )
+              );
+            })}
       </div>
     </div>
   );
