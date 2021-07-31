2d79349a3b76731ec02e8a5b1b01710b684278b5
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-31 10:47:44 -0700

    added route to retrieve all videos

--- Backend/routes/api/videoRoutes.js
+++ Backend/routes/api/videoRoutes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const videos = require("../../models/VideoSchema");
 
 // Search videos by topic - from params
-router.get("/:subject", async (req, res) => {
+router.get("/byTopic/:subject", async (req, res) => {
   let subject = req.params.subject;
   videos.find({ topic: subject }, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+router.get("/allVideos", async (req, res) => {
+  videos.find({}, (err, result) => {
     if (err) {
       throw err;
     } else {
