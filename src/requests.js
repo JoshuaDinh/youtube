011267627b26da632921c8b6137a47e186990e18
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-01 08:40:03 -0700

    added endpoints

--- src/requests.js
+++ src/requests.js
@@ -5,6 +5,9 @@ const requests = {
   Node: "api/videos/byTopic/Node",
   htmlcss: "api/videos/byTopic/Html-Css",
   allVideos: "api/videos/allVideos",
+  fillCommentDB: "api/dbFill/comments",
+  fetchCommentsById: "api/comments",
+  fetchVideoById: "api/view",
 };
 
 export default requests;
