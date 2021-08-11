4857e18c8a275132ae5d4aba83830e6aa4c5578f
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-11 07:51:52 -0700

    updated build

--- server.js
+++ server.js
@@ -29,11 +29,12 @@ app.use("/api/comments", commentsRoutes);
 
 
 // Serve static assets in production
-if (process.env.NODE_ENV == "production") {
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client" , "build" , "index.html"))
-})
-}
+// if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+// }
 
 app.listen(port, () => console.log(`app is running on ${port}`));
