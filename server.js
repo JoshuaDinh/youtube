3336566823b1806624bee8f0304b413dd4d21c7e
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-11 08:38:28 -0700

    updated

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/comments", commentsRoutes);
 // Serve static assets in production
 
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
