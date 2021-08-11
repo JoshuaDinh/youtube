ec609ae2c40530c0fde2bb39b490ccd709ec718a
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-11 08:54:18 -0700

    updated

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
 const connectDB = require("./config/db");
 const videoRoutes = require("./routes/api/videoRoutes");
-// const dbFillRoutes = require("./routes/api/dbFill");
 const commentsRoutes = require("./routes/api/commentsRoutes");
 
 // check/initialize process.env
@@ -15,10 +13,8 @@ if (result.error) {
 
 // initialize app config
 const app = express();
-app.use(cors());
 connectDB();
 
-const port = process.env.PORT || 5000;
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -31,9 +27,10 @@ app.use("/api/comments", commentsRoutes);
 // Serve static assets in production
 
   app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`app is running on ${port}`));
+app.listen(PORT, () => console.log(`app is running on ${PORT}`));
